c42845352830426f51ad1eaf688b0eb3cdc381d3
Florian Spiess <f.spiess@unibas.ch>
2019-12-31 11:40:15 +0100

    Updated c version to the same functionality as python version, just without the proper output formatting.

--- c/wgpu.c
+++ c/wgpu.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pwd.h>
 #include <nvml.h>
 
 // Constants
 
 #define PROCESS_NAME_BUFFER_SIZE 64
+#define USER_NAME_BUFFER_SIZE 64
 
-void getGpuInfo()
+typedef struct Process {
+    unsigned int pid;
+    char name[PROCESS_NAME_BUFFER_SIZE];
+    unsigned int uid;
+    char userName[USER_NAME_BUFFER_SIZE];
+} Process;
+
+typedef struct GPU {
+    unsigned int id;
+    char name[NVML_DEVICE_NAME_BUFFER_SIZE];
+    unsigned int numProcesses;
+    struct Process *processes;
+    unsigned int utilization;
+    unsigned long long usedMemory;
+    unsigned long long totalMemory;
+} GPU;
+
+typedef struct GPUList {
+    GPU *gpus;
+    unsigned int length;
+} GPUList;
+
+GPUList getGpuInfo()
 {
     nvmlReturn_t result;
-    unsigned int device_count, i;
+    unsigned int deviceCount, i;
     // Declare arrays for device information
-    // TODO
+    GPU *gpus = NULL;
+    GPUList gpuList;
 
     // Initialize NVML library
     result = nvmlInit();
@@ -21,23 +48,28 @@ void getGpuInfo()
         exit(1);
     }
 
-    result = nvmlDeviceGetCount(&device_count);
+    result = nvmlDeviceGetCount(&deviceCount);
     if (NVML_SUCCESS != result)
     { 
         printf("Failed to query device count: %s\n", nvmlErrorString(result));
         goto Error;
     }
-    printf("Found %d device%s\n", device_count, device_count != 1 ? "s" : "");
 
-    for (i = 0; i < device_count; i++)
+    // Allocate required memory for GPU structs
+    gpus = (GPU *)malloc(sizeof(GPU) * deviceCount);
+    gpuList.length = deviceCount;
+    gpuList.gpus = gpus;
+
+    for (i = 0; i < deviceCount; i++)
     {
         nvmlDevice_t device;
-        char name[NVML_DEVICE_NAME_BUFFER_SIZE];
         nvmlUtilization_t utilization;
         nvmlMemory_t memory;
-        unsigned long long usedMemory, totalMemory;
-        unsigned int numProcesses, j;
-        nvmlProcessInfo_t *processes;
+        unsigned int j;
+        nvmlProcessInfo_t *processes = NULL;
+
+        // Set GPU ID
+        gpus[i].id = i;
 
         // Get device handle
         result = nvmlDeviceGetHandleByIndex(i, &device);
@@ -48,7 +80,7 @@ void getGpuInfo()
         }
 
         // Get device name
-        result = nvmlDeviceGetName(device, name, NVML_DEVICE_NAME_BUFFER_SIZE);
+        result = nvmlDeviceGetName(device, gpus[i].name, NVML_DEVICE_NAME_BUFFER_SIZE);
         if (NVML_SUCCESS != result)
         { 
             printf("Failed to get name of device %i: %s\n", i, nvmlErrorString(result));
@@ -63,6 +95,9 @@ void getGpuInfo()
             goto Error;
         }
 
+        // Set GPU utilization
+        gpus[i].utilization = utilization.gpu;
+
         // Get device memory
         result = nvmlDeviceGetMemoryInfo(device, &memory);
         if (NVML_SUCCESS != result)
@@ -71,38 +106,46 @@ void getGpuInfo()
             goto Error;
         }
 
+        // Set GPU memory usage
+        gpus[i].usedMemory = memory.used;
+        gpus[i].totalMemory = memory.total;
+
         // Get device processes
-        numProcesses = 0;
-        result = nvmlDeviceGetComputeRunningProcesses(device, &numProcesses, NULL);
+        gpus[i].numProcesses = 0;
+        gpus[i].processes = NULL;
+        result = nvmlDeviceGetComputeRunningProcesses(device, &(gpus[i].numProcesses), NULL);
         if (result == NVML_ERROR_INSUFFICIENT_SIZE)
         {
-            processes = (nvmlProcessInfo_t *)malloc(sizeof(nvmlProcessInfo_t) * numProcesses);
-            nvmlDeviceGetComputeRunningProcesses(device, &numProcesses, processes);
+            processes = (nvmlProcessInfo_t *)malloc(sizeof(nvmlProcessInfo_t) * gpus[i].numProcesses);
+            result = nvmlDeviceGetComputeRunningProcesses(device, &(gpus[i].numProcesses), processes);
+            if (NVML_SUCCESS != result)
+            {
+                printf("Failed to get processes of device %i: %s\n", i, nvmlErrorString(result));
+                goto Error;
+            }
+
+            // Set GPU process info
+            gpus[i].processes = (Process *)malloc(sizeof(Process) * gpus[i].numProcesses);
+            for (j = 0; j < gpus[i].numProcesses; j++)
+            {
+                gpus[i].processes[j].pid = processes[j].pid;
+
+                result = nvmlSystemGetProcessName(processes[j].pid, gpus[i].processes[j].name, PROCESS_NAME_BUFFER_SIZE);
+                if (NVML_SUCCESS != result)
+                {
+                    printf("Failed to get process name of device %i process %i: %s\n", i, j, nvmlErrorString(result));
+                    goto Error;
+                }
+            }
+
+            // Free the allocated arrays
+            free(processes);
         }
         else if (NVML_SUCCESS != result)
         { 
-            printf("Failed to get processes of device %i: %s\n", i, nvmlErrorString(result));
+            printf("Failed to get number of processes of device %i: %s\n", i, nvmlErrorString(result));
             goto Error;
         }
-
-        usedMemory = memory.used / 1024 / 1024;
-        totalMemory = memory.total / 1024 / 1024;
-
-        printf("%d: %s (%d%%) [%lld MiB / %lld MiB] - %d processes\n", i, name, utilization.gpu, usedMemory, totalMemory, numProcesses);
-        for (j = 0; j < numProcesses; j++)
-        {
-            char process_name[PROCESS_NAME_BUFFER_SIZE];
-            result = nvmlSystemGetProcessName(processes[j].pid, process_name, PROCESS_NAME_BUFFER_SIZE);
-            if (NVML_SUCCESS != result)
-            { 
-                printf("Failed to get process name of device %i process %i: %s\n", i, j, nvmlErrorString(result));
-                goto Error;
-            }
-            printf("Process %d (PID: %d) has name: %s\n", j, processes[j].pid, process_name);
-        }
-
-        // Free the allocated arrays
-        free(processes);
     }
 
     // Shutdown NVML library
@@ -113,7 +156,7 @@ void getGpuInfo()
         exit(1);
     }
 
-    return;
+    return gpuList;
 
 Error:
     result = nvmlShutdown();
@@ -123,8 +166,96 @@ Error:
     exit(1);
 }
 
+unsigned int uidFromPid(unsigned int pid)
+{
+    struct stat stats;
+    char fileName[64];
+
+    sprintf(fileName, "/proc/%d", pid);
+
+    if (stat(fileName, &stats) == 0)
+    {
+        return stats.st_uid;
+    }
+
+    return -1;
+}
+
+void getUsernameFromUid(char *dest, unsigned int uid)
+{
+    struct passwd *pwd;
+
+    pwd = getpwuid(uid);
+
+    if (pwd)
+    {
+        strcpy(dest, pwd->pw_name);
+    }
+    else
+    {
+        strcpy(dest, "ERROR");
+    }
+}
+
+void updateProcessUserInfo(GPUList gpuList)
+{
+    unsigned int i, j;
+    for (i = 0; i < gpuList.length; i++)
+    {
+        for (j = 0; j < gpuList.gpus[i].numProcesses; j++)
+        {
+            gpuList.gpus[i].processes[j].uid = uidFromPid(gpuList.gpus[i].processes[j].pid);
+            if (gpuList.gpus[i].processes[j].uid < 0)
+            {
+                // UID retrieval has failed!
+                strcpy(gpuList.gpus[i].processes[j].userName, "ERROR");
+            }
+            else
+            {
+                getUsernameFromUid(gpuList.gpus[i].processes[j].userName, gpuList.gpus[i].processes[j].uid);
+            }
+        }
+    }
+}
+
+void printGpuList(GPUList gpuList)
+{
+    unsigned int i, j;
+    const char spacer[] = "   ";
+    GPU *gpu;
+    Process *process;
+
+    printf("GPU%sName%sUtil%sMemory Usage%sMemory Total%sIn Use\n", spacer, spacer, spacer, spacer, spacer);
+    printf("---%s----%s----%s------------%s------------%s------\n", spacer, spacer, spacer, spacer, spacer);
+
+    for (i = 0; i < gpuList.length; i++)
+    {
+        gpu = &(gpuList.gpus[i]);
+        printf("%d%s%s%s%d%%%s%lld MiB%s%lld MiB%s%s\n", gpu->id, spacer, gpu->name, spacer, gpu->utilization, spacer, gpu->usedMemory / 1024 / 1024, spacer, gpu->totalMemory / 1024 / 1024, spacer, gpu->numProcesses > 0 ? "Yes" : "No");
+    }
+
+    printf("\n");
+
+    printf("User%sGPU%sProcess\n", spacer, spacer);
+    printf("----%s---%s-------\n", spacer, spacer);
+
+    for (i = 0; i < gpuList.length; i++)
+    {
+        gpu = &(gpuList.gpus[i]);
+        for (j = 0; j < gpu->numProcesses; j++)
+        {
+            process = &(gpu->processes[j]);
+            printf("%s%s%d%s%s\n", process->userName, spacer, gpu->id, spacer, process->name);
+        }
+    }
+}
+
 int main()
 {
-    getGpuInfo();
+    GPUList gpuList;
+    gpuList = getGpuInfo();
+    updateProcessUserInfo(gpuList);
+    printGpuList(gpuList);
+
     return 0;
 }
